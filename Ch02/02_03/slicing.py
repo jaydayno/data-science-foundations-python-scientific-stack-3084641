187927b1b4a42024f9623710cd02c777e68dee06
jaydayno <119543960+jaydayno@users.noreply.github.com>
2022-12-23 12:49:31 +0000

    Initial commit

--- Ch02/02_03/slicing.py
+++ Ch02/02_03/slicing.py
@@ -1,6 +1,6 @@
 # %%
 nums = [1, 2, 3, 4, 5]
-nums[2:4]
+nums[2:5]
 
 # %%
 import numpy as np
--- Ch02/challenge/square.py
+++ Ch02/challenge/square.py
@@ -12,6 +12,26 @@ img.shape
 
 # %%
 # Draw a blue square around the flower
-# Top-left: 190x350
+# Top-left: 190x350 (y by x)
 # Bottom-right: 680x850
 # Line width: 5
+
+y_1, y_2 = 350, 850 # y-coords
+x_1, x_2 = 190, 680 # x-coords
+width = 5
+
+color = [0, 0 , 0xFF]
+
+#top of the square
+img[y_1:y_1+width, x_1:x_2] = color
+
+#left of the square
+img[y_1:y_2, x_1:x_1+width] = color
+
+#bottom of the square
+img[y_2:y_2+width, x_1:x_2] = color
+
+#right of the square
+img[y_1:y_2, x_2:x_2+width] = color
+plt.imshow(img)
+# %%
